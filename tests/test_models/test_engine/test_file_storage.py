9933ea0b46bb607cc3f802b5798fe9033538821a
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-12-01 10:38:37 -0400

    Adds unitest to file

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,7 @@
 """ Module for testing file storage"""
 import unittest
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 from models import storage
 import os
 
@@ -24,6 +25,10 @@ class test_fileStorage(unittest.TestCase):
         except:
             pass
 
+    def test_id(self):
+        """Test id"""
+        self.assertEqual(self.obj.id, id)
+
     def test_obj_list_empty(self):
         """ __objects is initially empty """
         self.assertEqual(len(storage.all()), 0)
@@ -40,6 +45,12 @@ class test_fileStorage(unittest.TestCase):
         new = BaseModel()
         temp = storage.all()
         self.assertIsInstance(temp, dict)
+
+    def test_Access(self):
+        """Test r-w-x access to file"""
+        self.assertTrue(os.access("models/engine/file_storage.py", os.R_OK))
+        self.assertTrue(os.access("models/engine/file_storage.py", os.W_OK))
+        self.assertFalse(os.access("models/engine/file_storage.py", os.X_OK))
 
     def test_base_model_instantiation(self):
         """ File is not created on BaseModel save """
@@ -59,6 +70,18 @@ class test_fileStorage(unittest.TestCase):
         new = BaseModel()
         storage.save()
         self.assertTrue(os.path.exists('file.json'))
+
+    def test_save_no_file(self):
+        """Test save method when the file does not exist"""
+        try:
+            os.remove(self.storage._FileStorage__file_path)
+        except FileNotFoundError:
+            pass
+        self.storage.new(self.obj)
+        self.storage.save()
+        key = self.obj.__class__.__name__ + "." + self.obj.id
+        with open(self.storage._FileStorage__file_path, "r") as f:
+            self.assertIn(key, f.read())
 
     def test_reload(self):
         """ Storage file is successfully loaded to __objects """
@@ -104,6 +127,43 @@ class test_fileStorage(unittest.TestCase):
 
     def test_storage_var_created(self):
         """ FileStorage object storage created """
-        from models.engine.file_storage import FileStorage
         print(type(storage))
         self.assertEqual(type(storage), FileStorage)
+
+    def test_Access(self):
+        """Test r-w-x access to file"""
+        self.assertTrue(os.access("models/engine/file_storage.py", os.R_OK))
+        self.assertTrue(os.access("models/engine/file_storage.py", os.W_OK))
+        self.assertFalse(os.access("models/engine/file_storage.py", os.X_OK))
+
+    def test_fundocs(self):
+        """Test if funtions have documentation"""
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
+
+    def test_do_create(self):
+        """Test the do_create method"""
+        # Test with no arguments
+        with self.assertRaises(SystemExit):
+            self.console.onecmd("create")
+
+        # Test with valid arguments
+        self.console.onecmd(
+            'create BaseModel name="My_little_house" number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 latitude=37.77 longitude=122.41')
+        all_objs = storage.all()
+        test_obj = list(all_objs.values())[-1]
+        self.assertEqual(test_obj.__class__.__name__, 'BaseModel')
+        self.assertEqual(test_obj.name, 'My little house')
+        self.assertEqual(test_obj.number_rooms, 4)
+        self.assertEqual(test_obj.number_bathrooms, 2)
+        self.assertEqual(test_obj.max_guest, 10)
+        self.assertEqual(test_obj.price_by_night, 300)
+        self.assertEqual(test_obj.latitude, 37.77)
+        self.assertEqual(test_obj.longitude, 122.41)
+
+
+if __name__ == "__main__":
+    unittest.main()
