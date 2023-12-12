84db66690eed5cb9850a449124b07baf624f6c5c
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-12-12 15:29:58 -0400

    Adds documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" This module starts a Flask web application """""
 from flask import Flask
-
+"""Flask class and render_template method"""
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+""" App route section"""
 
 
 @app.route('/')
