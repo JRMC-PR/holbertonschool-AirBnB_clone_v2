979a6b94bf3da77d820f2f31def4e1397f87d594
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-12-13 12:17:27 -0400

    Adds task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -40,8 +40,7 @@ def python(text='is cool'):
 
 @app.route('/number/<int:n>')
 def number(n):
-    if type(n) is int:
-        return f"{n} is a number"
+    return f"{n} is a number"
 
 
 if __name__ == '__main__':
